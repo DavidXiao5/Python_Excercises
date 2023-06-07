78ad0a721712be41b8e5e8453c5d7ab96164eec3
David Xiao <davexiao565@gmail.com>
2023-06-07 13:24:59 -0500

    VERSION 1.2 - Second Iteration of List_Find

--- List_Find.py
+++ List_Find.py
@@ -4,14 +4,9 @@ listC = [35, 56, 55]
 
 
 def findnums(used_list):
-    number_nineteen = used_list.count(19)
-    number_five = used_list.count(5)
-    if number_nineteen == 2 and number_five >= 3:
-        print(True)
-    else:
-        print(False)
+    return used_list.count(19) == 2 and used_list.count(5) >= 3
 
 
-findnums(listA)
-findnums(listB)
-findnums(listC)
+print(findnums(listA))
+print(findnums(listB))
+print(findnums(listC))
